84b63f22636ce3d91d6b99fdc1ea95d6ff87c22c
Abedalaziz Khalaf <44288084+abedalazizkhalaf@users.noreply.github.com>
2018-10-28 19:30:44 +0200

    Delete Practise1-C.py.py

--- Week 1/Practise1-C.py.py
+++
@@ -1,8 +0,0 @@
-#Assignment1 (abedalaziz khalaf)
-# Question 1 (C)
-C= float(input("Enter the tempreture in Celsius:"))
-F= 1.8*C + 32
-print("The tempreture in Fehrenheit is", F)
-F1= float(input("Enter the tempreture in Fehrenheit"))
-C1= (F1 - 32)/1.8
-print("The tempreture in Celsius is:", C1)
